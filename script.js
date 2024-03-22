befd120edc70bfb3359b4778f6f0f02553dd4d07
Aminul <141597421+Ultra-coder-Aminul@users.noreply.github.com>
2024-03-22 09:15:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function responseYes() {
+    document.getElementById("response").innerHTML = "I'm so happy to hear that! 😊";
+    // Replace 'YourNumber' with your actual number
+    window.location.href = "https://wa.me/9862463845";
+}
+
+function responseNo() {
+    document.getElementById("response").innerHTML = "I still love you. Please click 'Yes'.";
+
+    document.getElementById("hiddenYes").style.display = "block";
+    // Add a playful message when she types 'no'
+    alert("Oops! It seems like there was an error. Please try again by clicking 'Yes'.");
+}
+
+function freezeScreen() {
+    // Get the "No" button element
+    const noButton = document.getElementById('noButton');
+
+    // Add a click event listener to the "No" button
+    noButton.addEventListener('click', freezeScreen);
+
+    // Disable pointer events on the body element
+    document.body.style.pointerEvents = 'none';
+}
