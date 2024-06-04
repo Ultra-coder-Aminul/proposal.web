af412ca082386e1b78dfed587298fa128c09fb38
Aminul <141597421+Ultra-coder-Aminul@users.noreply.github.com>
2024-06-04 18:59:10 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function responseYes() {
     document.getElementById("response").innerHTML = "I'm so happy to hear that! 😊";
     // Replace 'YourNumber' with your actual number
-    window.location.href = "https://wa.me/";
+    window.location.href = "https://wa.me/9862463845";
 }
 
 function responseNo() {
